1344f75da0e0ecea0a93bcb5ff2d1244d2307610
Andre Calderon Martinez <al066228@uacam.mx>
2023-05-17 02:00:04 -0600

    Se agregaron opciones al usuario

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="index.php">
+                <a class="nav-link" href="regulation.php">
                     <i class="fas fa-fw fa-address-book"></i>
                     <span>Reglamento</span></a>
             </li>
@@ -267,13 +267,17 @@
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
+                                <!-- <a class="dropdown-item" href="#">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>
                                 <a class="dropdown-item" href="#">
                                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Settings
+                                </a> -->
+                                <a class="dropdown-item" href="#">
+                                    <i class="bi bi-box-seam-fill mr-2 text-gray-400"></i>
+                                    Inventory Log
                                 </a>
                                 <a class="dropdown-item" href="#">
                                     <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
